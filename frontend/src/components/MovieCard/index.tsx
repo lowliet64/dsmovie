4f403614c0727f0212653f418fec954191fc5a9a
Carlos Eduardo <kaduifrn@gmail.com>
2022-01-16 20:43:51 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -15,7 +15,7 @@ function MovieCard({ movie }: Props): JSX.Element {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <Link to={`/form/1${movie.id}`}>
+                <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>

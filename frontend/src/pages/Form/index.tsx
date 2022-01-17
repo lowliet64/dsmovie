089ebad68ee27ab5306aad4feb6d77c12392a9ef
Carlos Eduardo <kaduifrn@gmail.com>
2022-01-16 22:05:10 -0300

    fix: corrige erro no formulario

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,7 +1,7 @@
 
 import FormCard from 'components/FormCard';
-import { Link, useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
+import { useParams } from 'react-router-dom';
+
 
 
 function Form() {
